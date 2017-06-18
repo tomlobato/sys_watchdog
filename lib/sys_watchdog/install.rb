ccdc693ec7c93946b49bffb8a8605dd15e908485
Tom Lobato <tomlobato@gmail.com>
2017-06-18 22:15:52 +0000

    no message

--- lib/sys_watchdog/install.rb
+++ lib/sys_watchdog/install.rb
@@ -1,7 +1,38 @@
 
 class Install
-  def install
-    thisdir = File.join File.dirname(__FILE__)
+  def initialize
+    @thisdir = File.join File.dirname(__FILE__)
+  end
+
+  def install_with_systemd
+    copy_sample_conf
+    install_systemd_service
+
+    puts "Installed."
+
+    puts "\nEdit #{SysWatchdog::DEFAULT_CONF_FILE} and start:"
+    puts "systemctl start sys_watchdog"
+
+    puts "\nTo check daemon status:"
+    puts "systemctl status sys_watchdog"
+  end
+
+  def install_with_cron
+    copy_sample_conf
+    add_cron_line
+
+    puts "Installed."
+
+    puts "\nEdit #{SysWatchdog::DEFAULT_CONF_FILE} and uncomment the cron line added."
+  end
+  
+  private
+
+  def add_cron_line
+    run "echo '#* *   * * * root sys_watchdog' >> /etc/crontab"
+  end
+
+  def install_systemd_service
     services_dir = "/lib/systemd/system/"
 
     if `which systemctl`.empty?
@@ -14,24 +45,16 @@ class Install
       exit 1
     end
     
-    copy "#{thisdir}/../../util/sys_watchdog_sample.yml", 
-          SysWatchdog::DEFAULT_CONF_FILE
-
-    copy "#{thisdir}/../../util/sys_watchdog.service", 
+    copy "#{@thisdir}/../../util/sys_watchdog.service", 
          services_dir
 
     run 'systemctl enable sys_watchdog'
-
-    puts "Installed."
-
-    puts "\nEdit #{SysWatchdog::DEFAULT_CONF_FILE} and start:"
-    puts "systemctl start sys_watchdog"
-
-    puts "\nTo check daemon status:"
-    puts "systemctl status sys_watchdog"
   end
-  
-  private
+
+  def copy_sample_conf
+    copy "#{@thisdir}/../../util/sys_watchdog_sample.yml", 
+          SysWatchdog::DEFAULT_CONF_FILE
+  end
 
   def copy from, to
     puts "Copying #{from} to #{to}..."
@@ -42,4 +65,5 @@ class Install
     puts "Running #{cmd}..."
     system cmd
   end
+
 end
